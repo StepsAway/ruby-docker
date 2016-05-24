f0eea89964f3f691c3a2dc079171530fdf9fe339
JD Trout <jd.trout@stepsaway.com>
2016-05-24 11:36:26 -0700

    Locked in bundler version for 2.3

--- image/ruby2.3.0.sh
+++ image/ruby2.3.0.sh
@@ -5,9 +5,10 @@ source /rd_build/ruby_prep.sh
 source /rd_build/ruby_install
 set -x
 
-RUBY_MAJOR=2.3
-RUBY_VERSION=2.3.0
-RUBY_DOWNLOAD_SHA256=ba5ba60e5f1aa21b4ef8e9bf35b9ddb57286cb546aac4b5a28c71f459467e507
-RUBYGEMS_VERSION=2.6.4
+export RUBY_MAJOR=2.3
+export RUBY_VERSION=2.3.0
+export RUBY_DOWNLOAD_SHA256=ba5ba60e5f1aa21b4ef8e9bf35b9ddb57286cb546aac4b5a28c71f459467e507
+export RUBYGEMS_VERSION=2.6.4
+export BUNDLER_VERSION=1.12.4
 
 ruby_install
