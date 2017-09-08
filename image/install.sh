5d46e78ab5ef0dcc97c5c63e7a028c919c45ed0b
JD Trout <jd.trout@stepsaway.com>
2017-09-08 11:56:49 -0700

    [INFRA-255] Bump baseimage to 2.1.1

--- image/install.sh
+++ image/install.sh
@@ -5,11 +5,11 @@ set -x
 
 /rd_build/ruby_prep.sh
 
-if [[ "$ruby225" = 1 ]]; then /rd_build/ruby2.2.5.sh; fi
 if [[ "$ruby230" = 1 ]]; then /rd_build/ruby2.3.0.sh; fi
 if [[ "$ruby231" = 1 ]]; then /rd_build/ruby2.3.1.sh; fi
 if [[ "$ruby240" = 1 ]]; then /rd_build/ruby2.4.0.sh; fi
 if [[ "$ruby241" = 1 ]]; then /rd_build/ruby2.4.1.sh; fi
 
+cp /rd_build/rbenv-check.sh /usr/local/bin/rbenv-check.sh
 /rd_build/ruby_finalize.sh
 /rd_build/cleanup.sh
